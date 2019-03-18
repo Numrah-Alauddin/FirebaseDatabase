bf59b49f9f46fb4a22035199fc248ce2f3eac4e9
nalauddin <numrah55@gmail.com>
2019-03-18 11:02:06 +0500

    First commit

---
+++ app/src/main/java/com/example/firebasedatabase/Home.java
@@ -0,0 +1,73 @@
+package com.example.firebasedatabase;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class Home extends AppCompatActivity {
+
+    Button btn;
+    FirebaseAuth auth;
+    FirebaseDatabase database;
+    DatabaseReference reference;
+    TextView name_tv;TextView email_tv;
+    TextView id_tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        name_tv=findViewById(R.id.home_name);
+        id_tv=findViewById(R.id.home_id);
+        email_tv=findViewById(R.id.home_email);
+
+        btn=findViewById(R.id.login_logout_btn);
+        auth=FirebaseAuth.getInstance();
+        database=FirebaseDatabase.getInstance();
+        reference=database.getReference();
+
+        reference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                String id=dataSnapshot.child("id").getValue(String.class);
+                String name=dataSnapshot.child("name").getValue(String.class);
+                String email=dataSnapshot.child("email").getValue(String.class);
+
+                id_tv.setText(id);
+                email_tv.setText(email);
+                name_tv.setText(name);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                auth.signOut();
+                startActivity(new Intent(Home.this,Login.class));
+            }
+        });
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/firebasedatabase/MainActivity.java
@@ -0,0 +1,95 @@
+package com.example.firebasedatabase;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText email_et;
+    EditText pass_et;
+    EditText name_et;
+    Button signup;
+    FirebaseAuth auth;
+    FirebaseDatabase database;
+    DatabaseReference reference;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        init();
+
+
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email=email_et.getText().toString();
+                String pass=pass_et.getText().toString();
+                String name=name_et.getText().toString();
+
+               // signupUser(email,pass);
+
+
+                //First method
+             /*   reference.child("id").setValue("123");
+                reference.child("name").setValue("abc");
+                reference.child("email").setValue("email");*/
+
+             //2nd method
+                reference.child("id").setValue("123");
+                reference.child("name").setValue("abc");
+                reference.child("email").setValue("email");
+
+                startActivity(new Intent(MainActivity.this,Home.class));
+
+                email_et.setText("");
+                pass_et.setText("");
+
+            }
+        });
+    }
+
+    private void signupUser(String name,String pass) {
+
+        auth.createUserWithEmailAndPassword(name,pass).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    Toast.makeText(MainActivity.this, "Signup", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(MainActivity.this,Home.class));
+                }
+                else {
+                    Toast.makeText(MainActivity.this, task.getException().toString(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+
+    private void init() {
+
+        email_et=findViewById(R.id.email);
+        pass_et=findViewById(R.id.pass);
+        signup=findViewById(R.id.signup_btn);
+        name_et=findViewById(R.id.user_name);
+        auth=FirebaseAuth.getInstance();
+        database=FirebaseDatabase.getInstance();
+        reference=database.getReference("users");
+
+    }
+}
